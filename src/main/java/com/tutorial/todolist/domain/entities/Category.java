bd7c2c9af58accea96e6eaa69f454e99afb43e45
João Paulo <jpcchaves@outlook.com>
2023-06-18 19:42:01 -0300

    feat: add todo and category entities and relation one to many between them

---
+++ src/main/java/com/tutorial/todolist/domain/entities/Category.java
@@ -0,0 +1,55 @@
+package com.tutorial.todolist.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL,
+               fetch = FetchType.EAGER,
+               mappedBy = "category")
+    private List<Todo> todos = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(Long id,
+                    String name,
+                    List<Todo> todos) {
+        this.id = id;
+        this.name = name;
+        this.todos = todos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<Todo> todos) {
+        this.todos = todos;
+    }
+}
---
+++ src/main/java/com/tutorial/todolist/domain/entities/Todo.java
@@ -0,0 +1,120 @@
+package com.tutorial.todolist.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String todo;
+    private boolean concluded;
+    private boolean active;
+    private Date deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+
+    private Date concludedAt;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Todo() {
+    }
+
+    public Todo(Long id,
+                String todo,
+                boolean concluded,
+                boolean active,
+                Date deadline,
+                Category category,
+                Date concludedAt,
+                Date createdAt,
+                Date updatedAt) {
+        this.id = id;
+        this.todo = todo;
+        this.concluded = concluded;
+        this.active = active;
+        this.deadline = deadline;
+        this.category = category;
+        this.concludedAt = concludedAt;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public boolean isConcluded() {
+        return concluded;
+    }
+
+    public void setConcluded(boolean concluded) {
+        this.concluded = concluded;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Date getConcludedAt() {
+        return concludedAt;
+    }
+
+    public void setConcludedAt(Date concludedAt) {
+        this.concludedAt = concludedAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
