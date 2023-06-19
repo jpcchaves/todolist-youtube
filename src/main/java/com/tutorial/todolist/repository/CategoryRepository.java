868b61a6e5d5c530995849c98f458ecd9bf4f60f
João Paulo <jpcchaves@outlook.com>
2023-06-19 08:57:41 -0300

    feat: add category repository

---
+++ src/main/java/com/tutorial/todolist/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.tutorial.todolist.repository;
+
+import com.tutorial.todolist.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
