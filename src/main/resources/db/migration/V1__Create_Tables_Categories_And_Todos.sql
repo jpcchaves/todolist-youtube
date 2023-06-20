35d0b5215df1491bcf7652b338e555fddad4a940
João Paulo <jpcchaves@outlook.com>
2023-06-20 08:46:39 -0300

    feat: add migration create table categories and table todos

---
+++ src/main/resources/db/migration/V1__Create_Tables_Categories_And_Todos.sql
@@ -0,0 +1,30 @@
+CREATE TABLE
+    public.categories
+(
+    id   bigserial              NOT NULL,
+    name character varying(255) NULL
+);
+
+ALTER TABLE
+    public.categories
+    ADD
+        CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+CREATE TABLE
+    public.todos
+(
+    id           bigserial                      NOT NULL,
+    active       boolean                        NOT NULL,
+    concluded    boolean                        NOT NULL,
+    concluded_at timestamp(6) without time zone NULL,
+    created_at   timestamp(6) without time zone NULL,
+    deadline     timestamp(6) without time zone NULL,
+    todo         character varying(255)         NULL,
+    updated_at   timestamp(6) without time zone NULL,
+    category_id  bigint                         NULL
+);
+
+ALTER TABLE
+    public.todos
+    ADD
+        CONSTRAINT todos_pkey PRIMARY KEY (id);
