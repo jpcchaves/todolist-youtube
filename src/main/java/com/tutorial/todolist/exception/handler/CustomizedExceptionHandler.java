ab17f81ec0cfe50a8a10270784ca679ce3d572b8
João Paulo <jpcchaves@outlook.com>
2023-06-22 09:20:13 -0300

    feat: override handleMethodArgument not valid exception and extended ResponseEntityExceptionHandler

--- src/main/java/com/tutorial/todolist/exception/handler/CustomizedExceptionHandler.java
+++ src/main/java/com/tutorial/todolist/exception/handler/CustomizedExceptionHandler.java
@@ -2,19 +2,24 @@ package com.tutorial.todolist.exception.handler;
 
 import com.tutorial.todolist.exception.ResourceNotFoundException;
 import com.tutorial.todolist.exception.model.ExceptionResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 @ControllerAdvice
 @RestController
-public class CustomizedExceptionHandler {
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
     static Logger logger = Logger.getLogger("Exception");
 
     @ExceptionHandler(Exception.class)
@@ -34,5 +39,16 @@ public class CustomizedExceptionHandler {
                 request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), Objects.requireNonNull(
+                ex.getFieldError()).getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
